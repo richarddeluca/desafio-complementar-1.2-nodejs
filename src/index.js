efaa8ec846537ba22071ca0bcb3634b43324ebb6
Richard Deluca <richard.deluca.dev@gmail.com>
2022-06-27 14:55:12 -0300

    desafio concluído

--- src/index.js
+++ src/index.js
@@ -23,16 +23,18 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
-  return response.json(repository);
+  repositories.push(repository)
+
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const updatedRepository = request.body;
+  const { likes, ...updatedRepository } = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex < 0) {
+  if (repositoryIndex === -1) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -48,7 +50,7 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -65,10 +67,12 @@ app.post("/repositories/:id/like", (request, response) => {
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
-
+  console.log(repositories[repositoryIndex].likes)
   const likes = ++repositories[repositoryIndex].likes;
+  console.log(likes)
+  console.log(repositories[repositoryIndex].likes)
 
-  return response.json('likes');
+  return response.json(repositories[repositoryIndex]);
 });
 
 module.exports = app;
